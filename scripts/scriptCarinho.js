12160691384c58b831ebea837b92704615c038a0
MauroRaya <maurorayabr@gmail.com>
2025-04-28 22:40:29 -0300

    fix: update user time

--- scripts/scriptCarinho.js
+++ scripts/scriptCarinho.js
@@ -1,29 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    startTimer();
-});
-
-const atualizarPontuacao = async () => {
-  const nomeColuna = "tempo_fase3"; //MUDAR DE ACORDO COM O NOME DA COLUNA DO BANCO
-
-  if (userId) {
-      const baseUrl = 'https://feiraprofissoesunisanta2024-2-backend.onrender.com/usuario';
-
-      try {
-          const res = await fetch(baseUrl, {
-          method: "PUT",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ userId, nomeColuna, tempoFase }),
-          });
-
-          if (!res.ok) throw new Error((await res.json()).error);
-      } catch (error) {
-          console.error("Error:", error);
-      }
-  }
-}
-
 function moverCarrinho(valor) {
     const carrinho = document.getElementById('carrinho');
     const valorNumerico = parseFloat(valor); // Converte o valor para número
